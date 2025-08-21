83fd30772f852b6ccf6c0f167774a47b279aa6d9
Vishesh Raj <visheshrajput47@gmail.com>
2025-08-22 00:04:55 +0530

    done with view page

--- frontend/src/pages/ViewProfile.jsx
+++ frontend/src/pages/ViewProfile.jsx
@@ -7,7 +7,9 @@ import Logout from "../components/Header/Logout";
 
 const InfoDiv = ({info, icon}) => {
     return (
-         <motion.div className='flex flex-col items-start justify-center mt-2 p-6'>
+         <motion.div 
+         variants={itemVariants}
+         className='flex flex-col items-start justify-center mt-2 p-6'>
             <motion.h2
             className='flex items-center gap-x-2 text-sm sm:text-md md:text-lg'
             >
@@ -16,16 +18,37 @@ const InfoDiv = ({info, icon}) => {
          </motion.div>
     )
 }
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.3
+      }
+    }
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } }
+  };
+
+
 const ViewProfile = () => {
     const location = useLocation();
     const {userInfo} = location.state || {};
 
   return (
     <motion.div
+    variants={containerVariants}
     whileHover={{ scale: 1.05}}
-    className='w-full max-w-sm md:max-w-xl m-16 p-5 rounded-lg border-4 border-yellow-500 text-white backdrop-blur-md bg-white/20 mx-auto hover:shadow-lg hover:shadow-white'>
+    className='w-full max-w-sm md:max-w-xl m-16 p-5 rounded-lg border-4 border-yellow-500 text-white bg-gray-950 mx-auto hover:shadow-lg hover:shadow-white'>
          <h1 className=' text-lg sm:text-xl md:text-2xl text-emerald-300 font-bold flex items-center justify-center gap-x-2'><User2 color='lightGreen'/>Profile</h1>
-         <motion.div className='w-24 h-24 border-4 border-emerald-400 mt-6 bg-gray-900 rounded-2xl mx-auto flex items-center justify-center'>
+         <motion.div 
+         variants={itemVariants}
+         className='w-24 h-24 border-4 border-emerald-400 mt-6 bg-white rounded-2xl mx-auto flex items-center justify-center'>
             <motion.img
                 whileHover={{ scale: 1.1 }}
                 src={userInfo?.image || guideIcon}
@@ -37,7 +60,9 @@ const ViewProfile = () => {
          <InfoDiv info={userInfo?.email} icon={<Mail size={24} color='green'/>}/>
          <InfoDiv info={userInfo?.mobile || '000-000-000'} icon={<Phone size={24} color='red'/>}/>
 
-         <motion.div className='w-full flex items-center justify-around gap-x-4 mt-4'>
+         <motion.div
+         variants={itemVariants}
+          className='w-full flex items-center justify-around gap-x-4 mt-4'>
             <Link to='/edit-profile' className='flex items-center gap-x-2 text-sm sm:text-md md:text-lg bg-emerald-500 hover:bg-emerald-600 duration-500 p-2 rounded-lg'>
                <Pencil size={20}/> Edit Profile
             </Link>
