223d14d7e2624a4f846f602d16890a12f25f818f
Vishesh Raj <visheshrajput47@gmail.com>
2025-08-24 09:18:41 +0530

    some correction

--- frontend/src/pages/UpdateAlert.jsx
+++ frontend/src/pages/UpdateAlert.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import guideIcon from "../Assets/guideIcon.png";
+import guideIcon from "../assets/guideIcon.png";
 
 function Alert() {
   const [userData, setUserData] = useState({});
